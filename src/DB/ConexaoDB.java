c493c313fc1807988c40fad10ae1fafb0e375a9a
DalmyAlves <dalmyjr1@gmail.com>
2025-06-13 11:17:24 -0300

    add pasta DB

--- src/DB/ConexaoDB.java
+++ src/DB/ConexaoDB.java
@@ -1,4 +1,8 @@
 package DB;
 
 public class ConexaoDB {
+        private static final String URL = "";
+        private static final String USUARIO = "";
+        private static final String SENHA = "";
+
 }
