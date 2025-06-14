be5f34d5b7d188ad5182e610ad87b94aff236004
DalmyAlves <dalmyjr1@gmail.com>
2025-06-14 13:28:14 -0300

    Delete src directory

--- src/DB/ConexaoDB.java
+++
@@ -1,8 +0,0 @@
-package DB;
-
-public class ConexaoDB {
-        private static final String URL = "";
-        private static final String USUARIO = "";
-        private static final String SENHA = "";
-
-}
